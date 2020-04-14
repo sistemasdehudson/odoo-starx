35c1c722efaccf8ca0b1563fd6df769ef1d409cc
sistemassdeh <63413763+sistemassdeh@users.noreply.github.com>
2020-04-14 16:45:12 -0300

    Update __manifest__.py

--- sale_order_entrega_starx/__manifest__.py
+++ sale_order_entrega_starx/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/report.xml'
+        'views/report.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
